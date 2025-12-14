8d61a8cbaffbc5b2f209a99830cbce30c78c26f0
maugalcst <maugal.cst@gmail.com>
2025-12-13 21:33:38 -0600

    Agregar archivos de proyecto.

---
+++ BattleChat Arena/Models/PlayerState.cs
@@ -0,0 +1,13 @@
+﻿namespace BattleChat_Arena.Models
+{
+    public enum PlayerState
+    {
+        Normal = 0, 
+        Weakened = 1,
+        Freezed = 2,
+        Burning = 3, 
+        Confused = 4, 
+        Armored = 5, 
+        Camouflaged = 6
+    }
+}
---
+++ BattleChat Arena/Player.cs
@@ -0,0 +1,58 @@
+﻿using BattleChat_Arena.Models;
+
+namespace BattleChat_Arena
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public decimal Energy { get; set; }
+        public PlayerState CurrentState { get; set; }
+        public int StateTurnsRemaining { get; set; }
+        public bool IsConnected { get; set; }
+
+        public void ApplyStateEffect()
+        {
+            if (StateTurnsRemaining <= 0)
+            {
+                CurrentState = PlayerState.Normal;
+                return;
+            }
+            
+            switch (CurrentState)
+            {
+                case PlayerState.Burning:
+                    Health -= 5;
+                    break;
+
+                case PlayerState.Confused:
+                    break;
+
+                case PlayerState.Armored:
+                    break;
+
+            }
+
+            StateTurnsRemaining--;
+
+            if (StateTurnsRemaining <= 0)
+            {
+                CurrentState = PlayerState.Normal;
+            }
+                    
+        }
+
+        public bool CanExecuteCommand()
+        {
+            if (CurrentState == PlayerState.Freezed) 
+                return false;
+
+            else if (CurrentState == PlayerState.Armored && StateTurnsRemaining == 2)
+                return false;
+
+            else
+                return true; 
+        }
+
+    }
+}
